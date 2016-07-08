62ad31e40b80154df150ca20f1596aea6b5ad449
Tracy Isman <tracyisman@gmail.com>
2016-07-08 12:58:13 -0400

    Fixing the index view and the CRUD setup in restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,10 +5,45 @@ class RestaurantsController < ApplicationController
     end
 
     def create
-        @restaurant = Restaurant.new(params[:restaurant])
+        @restaurant = Restaurant.new(restaurant_params)
 
-        @restaurant.save
-        redirect_to @restaurant
+        if @restaurant.save
+            redirect_to @restaurant
+        else
+            render 'new'
+        end
     end
 
+    def show
+        @restaurant = Restaurant.find(params[:id])
+    end
+
+    def index
+        @restaurants = Restaurant.all
+    end
+
+    def edit
+        @restaurant = Restaurant.find(params[:id])
+    end
+
+    def update
+        @restaurant = Restaurant.find(params[:id])
+        if @restaurant.update(restaurant_params)
+            redirect_to @restaurant
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @restaurant = Restaurant.find(params[:id])
+        @restaurant.destroy
+        redirect_to restaurants_path
+    end
+
+    private
+
+    def restaurant_params
+        params.require(:restaurant).permit(:name, :description, :address, :phone)
+    end
 end
