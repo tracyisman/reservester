3d89744e01d2ea821bd9002096da062101e34144
Tracy Isman <tracyisman@gmail.com>
2016-07-08 13:35:59 -0400

    fixed the individual migrations, not the schema

--- db/migrate/20160708152318_create_users.rb
+++ db/migrate/20160708152318_create_users.rb
@@ -1,12 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :first_name
-      t.string :string
-      t.string :last_name
-      t.string :string
-      t.string :email
-      t.string :string
+      t.string  :name
+      t.integer :owner_id
     end
   end
 end
--- db/migrate/20160708161043_create_restaurants.rb
+++ db/migrate/20160708161043_create_restaurants.rb
@@ -2,9 +2,9 @@ class CreateRestaurants < ActiveRecord::Migration
   def change
     create_table :restaurants do |t|
       t.string :name
-      t.string :string
-      t.string :owner_id
-      t.string :integer
+      t.text   :description
+      t.string :address
+      t.string :phone
 
       t.timestamps null: false
     end
